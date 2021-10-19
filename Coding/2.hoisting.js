f788d73e7d1a8234f204243623101f22b177d732
Andrey <greemrock911@gmail.com>
2021-10-19 22:34:30 +0300

    feat: add questions

--- Coding/2.hoisting.js
+++ Coding/2.hoisting.js
@@ -1,24 +1,24 @@
-    // var
-// console.log(i) // undefined - потому что переменная var всплывает наверх и обявляется в самом начале, но без значения
-// var i = 52
-// console.log(i) // 52
+// var
+console.log(i) // undefined - потому что переменная var всплывает наверх и обявляется в самом начале, но без значения
+var i = 52
+console.log(i) // 52
 
 
-    // let, const
-// console.log(sum) // throws an error - because variables let and const aren't affected by hoisting
-// let sum = 42
-// console.log(sum) // 42
+// let, const
+console.log(sum) // throws an error - because variables let and const aren't affected by hoisting
+let sum = 42
+console.log(sum) // 42
 
-    // FUNCTION DECLARATION
-// console.log(sum(1, 2)) // 3 - потому что функция sum всплывает наверх и объявляется в самом начале
+// FUNCTION DECLARATION
+console.log(sum(1, 2)) // 3 - потому что функция sum всплывает наверх и объявляется в самом начале
 
-// function sum(a, b) {
-//     return a + b
-// } 
+function sum(a, b) {
+    return a + b
+} 
 
-    // FUNCTION EXPRESSION
-// console.log(square(5)) // returns an error, функиця square объявлена с помощью переменной, в таком случае на такую функция hoisting не действует
+// FUNCTION EXPRESSION
+console.log(square(5)) // returns an error, функиця square объявлена с помощью переменной, в таком случае на такую функция hoisting не действует
 
-// var square = function(num) {
-//     return num ** 2
-// }
+var square = function(num) {
+    return num ** 2
+}
